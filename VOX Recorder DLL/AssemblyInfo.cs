8000f1a51163872894095dc26d0e33d20080995f
Bob Aune <bob@proscan.org>
2022-07-03 21:59:55 -0700

    Delete AssemblyInfo.cs

--- VOX Recorder DLL/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("VOX Recorder DLL")]
-[assembly: AssemblyProduct("VOX Recorder DLL")]
-[assembly: AssemblyDescription("Support file for VOX Recorder")]
-[assembly: AssemblyVersion("1.0.0.0")] 
-[assembly: ComVisible(false)]
